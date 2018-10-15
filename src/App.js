b93bcaf7b349d18faaea55206f56b345ec1fc1df
yoidea <yoidea.cc@gmail.com>
2018-10-15 21:08:28 +0900

    Add router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Link, Redirect } from 'react-router-dom'
+import 'materialize-css';
+import 'materialize-css/dist/css/materialize.min.css';
+
+class ReactRouter extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/friends' component={Friends} />
+          <ul>
+            <li><Link className="waves-effect waves-light btn" to="/">Home</Link></li>
+            <li><Link className="waves-effect waves-light btn" to="/about">About</Link></li>
+            <li><Link className="waves-effect waves-light btn" to="/friends">Friends</Link></li>
+          </ul>
+        </div>
+      </BrowserRouter>
+    )
+  }
+}
+
+class Home extends Component {
+  render() {
+    return (
+      <div style={{background: "red"}}>
+        <h2>Home</h2>
+        <p>Welcome to</p>
+      </div>
+    )
+  } 
+}
+
+class About extends Component {
+  render() {
+    return (
+      <div style={{background: "green"}}>
+        <h2>About</h2>
+        <p>勘弁してください</p>
+      </div>
+    )
+  } 
+}
+
+class Friends extends Component {
+  render() {
+    return (
+      <div style={{background: "blue"}}>
+        <h2>Friends</h2>
+        <p>ここにフレンズのリストを書きます</p>
+      </div>
+    )
+  } 
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <ReactRouter />
       </div>
     );
   }
