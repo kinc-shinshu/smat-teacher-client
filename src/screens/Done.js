1ba9cb6f9de205df242944b88c020544e2192c58
yoidea <yoidea.cc@gmail.com>
2018-10-19 17:08:27 +0900

    Fix progressbar

--- src/screens/Done.js
+++ src/screens/Done.js
@@ -10,11 +10,19 @@ class Progress extends Component {
   }
 
   render() {
-    return (
-      <div className="progress">
-        <div className="determinate" style={{width: this.props.width + "%"}}></div>
-      </div>
-    );
+    let bar;
+    if (this.props.show){
+      bar = (
+        <div className="progress">
+          <div className="determinate" style={{width: this.props.width + "%"}}></div>
+        </div>
+      );
+    } else {
+      bar = (
+        <div></div>
+      );
+    }
+    return bar;
   }
 }
 
@@ -24,7 +32,8 @@ export class Done extends Component {
     super(props);
     this.state = {
       id: undefined,
-      progress: 0
+      progress: 0, 
+      barShow: true
     };
 
     const apiUrl = "https://smat-api.herokuapp.com"
@@ -59,9 +68,12 @@ export class Done extends Component {
             .then(json => {
               console.log(json);
               this.setState({progress: i / this.props.questions.length * 100});
+              console.log((i + 1) / this.props.questions.length * 100);
+              if ((i + 1) / this.props.questions.length == 1.0){
+                this.setState({barShow: false});
+              }
             });
         });
-        this.setState({progress: 100});
       });
     
   }
@@ -73,7 +85,7 @@ export class Done extends Component {
         <div className="container">
           <h2 className="center-align" style={{marginTop:"2em"}}>ルーム番号<span style={{fontSize:"3em"}} className="pink-text">{roomID}</span></h2>
           <h3 className="center-align hide-on-small-only" style={{marginBottom:"1em"}}>番号を生徒にお伝えください</h3>
-          <Progress width={this.state.progress} />
+          <Progress width={this.state.progress} show={this.state.barShow} />
         </div>
         <div className="center-align">
           <img src={tabletIcon} />
